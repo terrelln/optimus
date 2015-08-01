28c8aa51977625098278dcbf7542f92f3fc02bb9
Nick Terrell <terrelln@umich.edu>
2015-07-31 21:27:30 -0700

    Add some helper metafunctions.

---
+++ traits.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <type_traits>
+
+namespace optimus {
+
+// Remove once C++ 14 is here.
+template <typename T>
+using result_of_t = typename ::std::result_of<T>::type;
+
+template <typename, typename...>
+struct safe_forwarding_constructor : ::std::true_type { };
+
+template <typename Class, typename T>
+struct safe_forwarding_constructor<Class, T> :
+    ::std::integral_constant<
+        bool,
+        !std::is_base_of<
+            Class,
+            typename ::std::decay<T>::type
+        >::value
+    >
+{ };
+
+template <typename Class, typename... Args>
+using safe_forwarding_constructor_t = typename ::std::enable_if<
+    safe_forwarding_constructor<Class, Args...>::value
+>::type;
+
+
+}
